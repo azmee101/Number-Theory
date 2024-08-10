948738c8871c01838fdd870d34aec3eec80c4d7f
azmee101 <99961135+azmee101@users.noreply.github.com>
2024-08-10 17:37:46 +0600

    Update complexity div_sum.cpp

--- div_sum.cpp
+++ div_sum.cpp
@@ -12,7 +12,7 @@ const int N = 500001;
 ll arr[N];
 
 void divisors_sum(){
-    for(int i = 1; i <= N-1; i++){
+    for(int i = 1; i <= (N/2 + 1); i++){
         for(int j = i+i; j <= N-1; j+=i){
             arr[j] += i;
         }
